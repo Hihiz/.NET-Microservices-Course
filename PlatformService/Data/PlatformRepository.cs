bdad731b9f14debeb42b8aaa094cd9febb651611
Hihiz <public@mail.ru>
2023-08-04 01:17:22 +0300

    Хранение данных

---
+++ PlatformService/Data/PlatformRepository.cs
@@ -0,0 +1,27 @@
+﻿using PlatformService.Models;
+
+namespace PlatformService.Data
+{
+    public class PlatformRepository : IPlatformRepository
+    {
+        private readonly ApplicationDbContext _db;
+
+        public PlatformRepository(ApplicationDbContext db) => _db = db;
+
+        public void CreatePlatform(Platform platform)
+        {
+            if (platform == null)
+            {
+                throw new ArgumentNullException(nameof(platform));
+            }
+
+            _db.Platforms.Add(platform);
+        }
+
+        public IEnumerable<Platform> GetAllPlatforms() => _db.Platforms.ToList();
+
+        public Platform GetPlatformById(int id) => _db.Platforms.FirstOrDefault(p => p.Id == id);
+
+        public bool SaveChanges() => _db.SaveChanges() >= 0;
+    }
+}
