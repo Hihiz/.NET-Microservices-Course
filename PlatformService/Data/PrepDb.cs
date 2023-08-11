46372238c5e98fddf1e9d85d6b7384b5767a6cf6
Hihiz <public@mail.ru>
2023-08-12 02:12:56 +0300

    Начальные данные

--- PlatformService/Data/PrepDb.cs
+++ PlatformService/Data/PrepDb.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Identity.Client;
+using PlatformService.Models;
 
 namespace PlatformService.Data
 {
@@ -8,13 +9,42 @@ namespace PlatformService.Data
         {
             using (var serviceScope = app.ApplicationServices.CreateScope())
             {
-
+                SeedData(serviceScope.ServiceProvider.GetService<ApplicationDbContext>());
             }
         }
 
         public static void SeedData(ApplicationDbContext db)
         {
+            if (!db.Platforms.Any())
+            {
+                Console.WriteLine("--> Seeding Data");
 
+                db.Platforms.AddRange(
+                    new Platform()
+                    {
+                        Name = "Dot Net",
+                        Publisher = "Microsoft",
+                        Cost = "Free"
+                    },
+                    new Platform()
+                    {
+                        Name = "SQL Server Express",
+                        Publisher = "Microsoft",
+                        Cost = "Free"
+                    },
+                    new Platform()
+                    {
+                        Name = "Kubernetes",
+                        Publisher = "Could Native Computing Foundation",
+                        Cost = "Free"
+                    });
+
+                db.SaveChanges();
+            }
+            else
+            {
+                Console.WriteLine("--> We already have data");
+            }
         }
     }
 }
