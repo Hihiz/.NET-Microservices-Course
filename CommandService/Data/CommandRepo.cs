b7ca80b54e482789aa3a71250ccba4a51fd85bae
Hihiz <public@mail.ru>
2023-08-17 04:13:31 +0300

    Репозиторий, дто, автомаппера

---
+++ CommandService/Data/CommandRepo.cs
@@ -0,0 +1,60 @@
+﻿using CommandService.Models;
+
+namespace CommandService.Data
+{
+    public class CommandRepo : ICommandRepo
+    {
+        private readonly ApplicationDbContext _db;
+
+        public CommandRepo(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public void CreateCommand(int platformId, Command command)
+        {
+            if (command == null)
+            {
+                throw new ArgumentNullException(nameof(command));
+            }
+
+            command.PlatformId = platformId;
+            _db.Commands.Add(command);
+        }
+
+        public void CreatePlatform(Platform platform)
+        {
+            if (platform == null)
+            {
+                throw new ArgumentNullException(nameof(platform));
+            }
+
+            _db.Platforms.Add(platform);
+        }
+
+        public IEnumerable<Platform> GetAllPlatforms()
+        {
+            return _db.Platforms.ToList();
+        }
+
+        public Command GetCommand(int platformId, int commandId)
+        {
+            return _db.Commands.Where(c => c.PlatformId == platformId && c.Id == commandId).FirstOrDefault();
+        }
+
+        public IEnumerable<Command> GetCommandsForPlatform(int platformId)
+        {
+            return _db.Commands.Where(c => c.PlatformId == platformId).OrderBy(c => c.Platform.Name);
+        }
+
+        public bool PlatformExits(int platformId)
+        {
+            return _db.Platforms.Any(p => p.Id == platformId);
+        }
+
+        public bool SaveChanges()
+        {
+            return (_db.SaveChanges() >= 0);
+        }
+    }
+}
